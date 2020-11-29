1f2ed9a0701c04eb2d4a36b4b28afd5a754426e9
sandeepcpatil <sandeepcpatil1998@gmail.com>
2020-11-29 10:13:39 +0530

    1 file modified

--- index.js
+++ index.js
@@ -6,18 +6,20 @@ function AddForm() {
   const [num, setNum] = useState(0);
   const [sum, setSum] = useState(0);
 
-  function inputReceive(e) {
+  function handleChange(e) {
     setNum(e.target.value);
   }
-  function addNum(num) {
-    return num;
+  function handleSubmit(e){
+    setSum(sum + Number(num))
+    e.preventDefault()
   }
 
   return (
-    <div>
-      <input type="text" onChange={inputReceive} />
-      <button>Add</button>
-    </div>
+  <form onSubmit={handleSubmit}>
+  <input type="number" value={num} onChange={handleChange} />
+  <input type="submit" value="Add" />
+  <p> Sum is {sum} </p>
+  </form>
   );
 }
 
